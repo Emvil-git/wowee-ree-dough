fd41b562cfa875595bf1e9ffea163270b4f66567
Emvil-git <emvil542@gmail.com>
2026-01-25 18:47:15 +0800

    feat: change tx type to use cat unique name i/of whole category obj

--- src/types/transactionType.ts
+++ src/types/transactionType.ts
@@ -1,9 +1,7 @@
-import type { Category } from "./category";
-
 export interface TransactionType {
     id: number;
     name: string;
     value: number;
     date: string;
-    category: Category
+    categoryUname: string
 }
