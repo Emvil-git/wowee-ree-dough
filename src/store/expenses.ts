9fec9bbfe44c6b4a13d691616a7bd610c32af62b
Emvil-git <emvil542@gmail.com>
2026-02-04 20:28:57 +0800

    feat: add persist in zustand store to save in localstorage

--- src/store/expenses.ts
+++ src/store/expenses.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import type { StateCreator } from "zustand";
+import { persist } from 'zustand/middleware';
 import type { TransactionType } from "../types/transactionType";
 import type { Category } from "../types/category";
 
@@ -37,38 +38,75 @@ const MOCKtx: TransactionType[] = [
 ]
 
 interface TransactionSlice {
-    transactions: TransactionType[]
+    transactions: TransactionType[],
+    setTransactions: (newTxs: TransactionType[]) => void 
     addTransaction: (newTx: TransactionType) => void
     deleteTransaction: (delId: bigint) => void
     editTransaction: (id : bigint, updatesTx: Partial<TransactionType>) => void
 }
 
 interface CategorySlice {
-    categories: Category[]
+    categories: Category[],
+    setCategories: (newCats: Category[]) => void,
     addCategory: (newCat: Category) => void
     deleteCategory: (delUName: string) => void
     editCategory: (uniqueName: string, updatesCat: Partial<Category>) => void //ts will change once you add a colour and icon to the category type
 }
 
-const createTransactionSlice: StateCreator<TransactionSlice, []> = (set) => ({
+// const createTransactionSlice: StateCreator<TransactionSlice,[["zustand/persist", unknown]], []> = (set) => ({
+//     transactions: MOCKtx,
+//     setTransactions: (newTxs) => set({ transactions: newTxs }),
+//     addTransaction: (newTx) => set((state) => ({ transactions: [...state.transactions, newTx]})),
+//     deleteTransaction: (delId) => set((state) => ({ transactions: state.transactions.filter((tx: TransactionType) => tx.id !== delId)})),
+//     editTransaction: (upId, updates) => set((state) => ({ transactions: state.transactions.map((tx: TransactionType) => tx.id === upId ? {...tx, ...updates} : tx ) }))
+// })
+
+
+const createTransactionSlice: StateCreator<TransactionSlice & CategorySlice,[["zustand/persist", unknown]], [], TransactionSlice> = (set) => ({
     transactions: MOCKtx,
+    setTransactions: (newTxs) => set({ transactions: newTxs }),
     addTransaction: (newTx) => set((state) => ({ transactions: [...state.transactions, newTx]})),
     deleteTransaction: (delId) => set((state) => ({ transactions: state.transactions.filter((tx: TransactionType) => tx.id !== delId)})),
     editTransaction: (upId, updates) => set((state) => ({ transactions: state.transactions.map((tx: TransactionType) => tx.id === upId ? {...tx, ...updates} : tx ) }))
 })
 
-const createCategorySlice: StateCreator <CategorySlice, []> = (set) => ({
+// const createCategorySlice: StateCreator <CategorySlice,[["zustand/persist", unknown]], []> = (set) => ({
+//     categories: defaultCategories,
+//     setCategories: (newCats) => set({ categories: newCats }),
+//     addCategory: (newCat) => set((state) => ({ categories: [...state.categories, newCat] })),
+//     deleteCategory: (delUName) => set((state) => ({ categories: state.categories.filter((cat: Category) => cat.uniqueName !== delUName) })),
+//     editCategory: (uniqueName, upCat) => set((state) => ({categories: state.categories.map( (cat: Category) => cat.uniqueName === uniqueName ? {...cat, ...upCat} : cat ) }) )
+// })
+
+const createCategorySlice: StateCreator <TransactionSlice & CategorySlice,[["zustand/persist", unknown]], [], CategorySlice> = (set) => ({
     categories: defaultCategories,
+    setCategories: (newCats) => set({ categories: newCats }),
     addCategory: (newCat) => set((state) => ({ categories: [...state.categories, newCat] })),
     deleteCategory: (delUName) => set((state) => ({ categories: state.categories.filter((cat: Category) => cat.uniqueName !== delUName) })),
     editCategory: (uniqueName, upCat) => set((state) => ({categories: state.categories.map( (cat: Category) => cat.uniqueName === uniqueName ? {...cat, ...upCat} : cat ) }) )
 })
 
+// const useExStore = create<TransactionSlice & CategorySlice>()(
+//     (...args) => ({
+//         ...createTransactionSlice(...args),
+//         ...createCategorySlice(...args)
+//     })
+// )
+
 const useExStore = create<TransactionSlice & CategorySlice>()(
-    (...args) => ({
-        ...createTransactionSlice(...args),
-        ...createCategorySlice(...args)
-    })
+    persist(
+        (...args) => ({
+            ...createTransactionSlice(...args),
+            ...createCategorySlice(...args)
+        }),
+        {
+            name: 'expense-storage',
+            partialize: (state) => ({
+                transactions: state.transactions,
+                categories: state.categories
+            })
+        }
+    )
 )
 
 export default useExStore
