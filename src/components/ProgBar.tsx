e9bef726e8815b74703c61fc42e1ef859c3e1242
Emvil-git <emvil542@gmail.com>
2026-02-09 18:17:44 +0800

    feat: show total spent on the top of home

--- src/components/ProgBar.tsx
+++ src/components/ProgBar.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react"
+import useExStore from "../store/expenses"
 
 export const ProgBar = () => {
     const [max, setMax] = useState(100)
     const [fill, setFill] = useState(40)
 
+    const transactions = useExStore((state => state.transactions))
+
+    const total = transactions.length ? transactions.map(
+        (tx) => tx.value
+    ).reduce(
+        (acc, val) => acc + val
+    ) : 0
+    
+
     return (
         <div className="w-full max-w-[32em]">
-            {fill}/{max}
+            {total}/{max}
         </div>
     )
 }
--- src/pages/Transactions/Transactions.tsx
+++ src/pages/Transactions/Transactions.tsx
@@ -1,7 +1,28 @@
+import { useMemo } from "react"
+import useExStore from "../../store/expenses"
+import TransacItem from "../../components/TransacItem"
+
 const Transactions = () => {
+
+    const transactions = useExStore((state) => state.transactions)
+
+    const sortedTx = useMemo(
+        () => {
+            return [...transactions].sort((a,b) => b.date.localeCompare(a.date))
+        },[transactions]
+    )
+
     return(
         <div>
-            This is the transactions page
+            <div>
+                TODAY
+                <span>sort/ group by</span>
+            </div>
+            <div>
+                {sortedTx.map((tx) => 
+                    <TransacItem key={tx.id} {...tx} canUD={false}/>
+                )}
+            </div>
         </div>
     )
 }
